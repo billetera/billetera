a1c7fdd5b3fd320094a9e4fa2bce0e5270072a18
Sandeep Joseph <sandeep.joseph@sap.com>
2017-02-20 09:44:56 +0100

    Initial Upload - Project Creation

---
+++ src/test/java/com/billetera/BilleteraApplicationTests.java
@@ -0,0 +1,16 @@
+package com.billetera;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BilleteraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
